e4145cb4c17598a2f3405bd3c2f729010821588e
bei <782753777@qq.com>
2024-03-27 23:03:33 +0800

    fix: 🐛  project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  rules: {
+    'no-debugger': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error'
+  },
+  overrides: [
+    {
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ stylelint.config.cjs
@@ -0,0 +1,5 @@
+/*eslint-env node*/
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: 'stylelint-config-recommended-vue'
+}
